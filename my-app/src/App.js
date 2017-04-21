3b868be77440a40f3b3cc56ac51ee564960c64fc
Unknown <adrusnak@gmail.com>
2017-04-21 15:52:14 +0200

    new component created Adam

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class Adam extends Component {
+	
+	render(){
+		return(
+		
+		<div>Adam Rusnak</div>
+		
+		);
+	}
+	
+}
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +26,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+		
+		<Adam> </Adam>
+		
       </div>
     );
   }
