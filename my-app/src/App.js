2004cedde2a0a94863adb7e0401e951eb0c7650f
Unknown <adrusnak@gmail.com>
2017-05-12 15:48:33 +0200

    removed unnecessary shit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
+class Konrad extends Component {
 
-class Konrad extends Component{
-	
-	render(){
-		return(
-			<div>Konrad Hanus is Awesome</div>
-		);
-	}
+    render() {
+        return (
+            <div>Konrad Hanus is Awesome</div>
+        );
+    }
 }
 
 class Adam extends Component {
-	
-	render(){
-		return(
-		
-		<div>Adam Rusnak</div>
-		
-		);
-	}
-	
+
+    render() {
+        return (
+
+            <div>Adam Rusnak</div>
+
+        );
+    }
+
 }
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-		
-		<Adam> </Adam>
-		
-		<Konrad />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+
+                <Adam></Adam>
+
+                <Konrad/>
+            </div>
+        );
+    }
 }
 
 export default App;
