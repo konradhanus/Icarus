15228caa2ff2882c29052a9977d4956278ed121c
Unknown <adrusnak@gmail.com>
2017-05-12 15:53:31 +0200

    main component renamed

---
+++ my-app/src/Icarus.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Icarus extends Component {
+    render() {
+        return (
+            <div className="Icarus"></div>
+        );
+    }
+}
+
+export default Icarus;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Icarus from './Icarus';
 
 ReactDOM.render(
-    <App/>, document.getElementById('root'));
+    <Icarus/>, document.getElementById('root'));
