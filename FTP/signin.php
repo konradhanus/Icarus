a499a049a8977b839d154f8759951374df9abeea
konrad.hanus <konrad.hanus@gmail.com>
2017-11-13 13:01:11 +0100

    session added in signin.php

--- FTP/signin.php
+++ FTP/signin.php
@@ -7,27 +7,20 @@ header('Access-Control-Allow-Origin: *');
 require_once('sql.php');
  $userName = $_GET['userName'];
  $userPassword = md5($_GET['userPassword']);
- 
 
-
- $return = 0;
+ $is_logged_in = 0;
 
 
  if (!$zaptanie=@mysql_query("SELECT `id` FROM `users` WHERE `email`='$userName' AND `password`='$userPassword'")) {
             echo 'Bład zapytania MySQL, odpowiedź serwera: '.mysql_error(); 
-        }
+}
         else
         {
-
-
             while ($wiersze = mysql_fetch_object($zaptanie)) 
             {
+                $is_logged_in = 1;
+            } 
                 
-                $return = 1;
-                 
-            
-             } 
-                $is_logged_in = $return;
 
                 if(is_null($is_logged_in))
                 {
@@ -35,6 +28,7 @@ require_once('sql.php');
                 }
 
                 $return2 = array('is_logged_in' => $is_logged_in);
+                $_SESSION['is_logged_in'] = $is_logged_in;
                 echo json_encode($return2);
 			/* 
 			$_SESSION['is_logged_in'] = $return;
