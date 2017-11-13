c628b108c1696f19b41a2a600ec486073f5d179a
konrad.hanus <konrad.hanus@gmail.com>
2017-11-13 13:40:52 +0100

    loggedin ws

--- FTP/checkifloggedin.php
+++ FTP/checkifloggedin.php
@@ -1,7 +1,8 @@
 <?php
 header('Content-Type: application/json;  charset=utf-8');
+header('Access-Control-Allow-Origin: *');
 session_start();
 
-echo json_encode($_SESSION['id']);
+echo json_encode($_SESSION['is_logged_in']);
 
 ?>
